c499ed4c0490aa08b8d93416cc963b13758de680
parkgmin <rudlas0114@naver.com>
2020-11-14 11:52:10 +0900

    modify korean

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from pyowm import OWM 
 
 k2c = lambda k: k - 273.15
-API_KEY = "당신의 키를 적어주세요" 
+API_KEY = "당신의 키를 입력하세요" 
 owm = OWM(API_KEY) 
 
 mgr = owm.weather_manager() 
-obs = mgr.weather_at_place('Seoul,KR') 
+obs = mgr.weather_at_place('Seoul,KR')
+
 weather = obs.weather
 temp = weather.temperature() 
 print(weather.detailed_status) 
 
-print("최대 온도:", k2c(temp["temp_max"])) 
-print("최소 온도:", k2c(temp["temp_min"])) 
+
+print("최고 온도:", k2c(temp["temp_max"])) 
+print("최저 온도:", k2c(temp["temp_min"])) 
+
+
+#print("최대 온도:", k2c(temp["temp_max"])) 
+#print("최소 온도:", k2c(temp["temp_min"])) 
